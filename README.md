e4424240faed2b8f3178a9cb0d6ef2285d3453c8
Uri Herrera <hello@uriherrera.com>
2022-08-03 03:14:19 -0500

    add deb workflow

---
+++ README.md
@@ -0,0 +1,8 @@
+# Cask Server
+
+Public server and API to interface with Cask features .
+
+# Issues
+If you find problems with the contents of this repository please create an issue.
+
+©2022 Nitrux Latinoamericana S.C.
---
+++ scripts/build-deb.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+
+set -x
+
+### Update sources
+
+wget -qO /etc/apt/sources.list.d/neon-user-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.neon.user
+
+wget -qO /etc/apt/sources.list.d/nitrux-main-compat-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux
+
+wget -qO /etc/apt/sources.list.d/nitrux-testing-repo.list https://raw.githubusercontent.com/Nitrux/iso-tool/development/configs/files/sources.list.nitrux.testing
+
+DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver keyserver.ubuntu.com --recv-keys \
+	55751E5D > /dev/null
+
+curl -L https://packagecloud.io/nitrux/repo/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/compat/gpgkey | apt-key add -;
+curl -L https://packagecloud.io/nitrux/testing/gpgkey | apt-key add -;
+
+DEBIAN_FRONTEND=noninteractive apt -qq update
+
+### Install Package Build Dependencies #2
+
+DEBIAN_FRONTEND=noninteractive apt -qq -yy install --no-install-recommends \
+	maui-manager-git
+
+### Download Source
+
+git clone --depth 1 --branch $CASKSERVER_BRANCH https://github.com/Nitrux/cask-server.git
+
+### Compile Source
+
+mkdir -p build && cd build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=/usr \
+	-DENABLE_BSYMBOLICFUNCTIONS=OFF \
+	-DQUICK_COMPILER=ON \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_SYSCONFDIR=/etc \
+	-DCMAKE_INSTALL_LOCALSTATEDIR=/var \
+	-DCMAKE_EXPORT_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_FIND_PACKAGE_NO_PACKAGE_REGISTRY=ON \
+	-DCMAKE_INSTALL_RUNSTATEDIR=/run "-GUnix Makefiles" \
+	-DCMAKE_VERBOSE_MAKEFILE=ON \
+	-DCMAKE_INSTALL_LIBDIR=lib/x86_64-linux-gnu ../cask-server/
+
+make -j$(nproc)
+
+### Run checkinstall and Build Debian Package
+
+>> description-pak printf "%s\n" \
+	'Public server and API to interface with Cask features.' \
+	'' \
+	''
+
+checkinstall -D -y \
+	--install=no \
+	--fstrans=yes \
+	--pkgname=cask-server-git \
+	--pkgversion=$PACKAGE_VERSION \
+	--pkgarch=amd64 \
+	--pkgrelease="1" \
+	--pkglicense=LGPL-3 \
+	--pkggroup=libs \
+	--pkgsource=cask-server \
+	--pakdir=. \
+	--maintainer=probal31@gmail.com \
+	--provides=cask-server \
+	--requires="libc6,maui-manager-git \(\>= 2.2.0+git\)" \
+	--nodoc \
+	--strip=no \
+	--stripso=yes \
+	--reset-uids=yes \
+	--deldesc=yes
